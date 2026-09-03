5e48ba86c7c4afda21b828881701fba3f93a9881
Muhammad Syaifulloh <msyasy.care@gmail.com>
2026-09-03 19:45:03 +0700

    fix: switch SMTP connection to SSL port 465

--- utils/email.go
+++ utils/email.go
@@ -1,6 +1,7 @@
 package utils
 
 import (
+	"crypto/tls"
 	"fmt"
 	"log"
 	"net/smtp"
@@ -14,7 +15,6 @@ func SendResetPasswordEmail(toEmail, token string) error {
 	smtpPassword := os.Getenv("SMTP_PASSWORD")
 	frontendURL := os.Getenv("FRONTEND_URL")
 
-	// 1. Validasi variabel environment
 	if smtpHost == "" || smtpPort == "" || smtpEmail == "" || smtpPassword == "" {
 		err := fmt.Errorf("variabel SMTP belum lengkap di environment")
 		log.Println("[SMTP ERROR]:", err)
@@ -23,7 +23,6 @@ func SendResetPasswordEmail(toEmail, token string) error {
 
 	resetLink := fmt.Sprintf("%s/reset-password?token=%s", frontendURL, token)
 
-	// 2. Format Header & Content Email (Lengkap dengan From dan To)
 	headers := make(map[string]string)
 	headers["From"] = fmt.Sprintf("Aplikasi Keuangan <%s>", smtpEmail)
 	headers["To"] = toEmail
@@ -53,15 +52,61 @@ func SendResetPasswordEmail(toEmail, token string) error {
 
 	message += "\r\n" + body
 
-	// 3. Autentikasi dan Pengiriman Email
-	auth := smtp.PlainAuth("", smtpEmail, smtpPassword, smtpHost)
 	addr := fmt.Sprintf("%s:%s", smtpHost, smtpPort)
+	log.Printf("[SMTP INFO] Mengirim email via SSL ke %s (%s)...", toEmail, addr)
 
-	log.Printf("[SMTP INFO] Mengirim email reset password ke %s via %s...", toEmail, addr)
+	auth := smtp.PlainAuth("", smtpEmail, smtpPassword, smtpHost)
 
-	err := smtp.SendMail(addr, auth, smtpEmail, []string{toEmail}, []byte(message))
+	// Dial menggunakan TLS/SSL langsung untuk port 465
+	tlsconfig := &tls.Config{
+		InsecureSkipVerify: false,
+		ServerName:         smtpHost,
+	}
+
+	conn, err := tls.Dial("tcp", addr, tlsconfig)
 	if err != nil {
-		log.Println("[SMTP ERROR Gagal Kirim Email]:", err)
+		log.Println("[SMTP ERROR TLS Dial Failed]:", err)
+		return err
+	}
+	defer conn.Close()
+
+	client, err := smtp.NewClient(conn, smtpHost)
+	if err != nil {
+		log.Println("[SMTP ERROR NewClient Failed]:", err)
+		return err
+	}
+	defer client.Quit()
+
+	if err = client.Auth(auth); err != nil {
+		log.Println("[SMTP ERROR Auth Failed]:", err)
+		return err
+	}
+
+	if err = client.Mail(smtpEmail); err != nil {
+		log.Println("[SMTP ERROR Mail From Failed]:", err)
+		return err
+	}
+
+	if err = client.Rcpt(toEmail); err != nil {
+		log.Println("[SMTP ERROR Rcpt To Failed]:", err)
+		return err
+	}
+
+	w, err := client.Data()
+	if err != nil {
+		log.Println("[SMTP ERROR Data Failed]:", err)
+		return err
+	}
+
+	_, err = w.Write([]byte(message))
+	if err != nil {
+		log.Println("[SMTP ERROR Write Failed]:", err)
+		return err
+	}
+
+	err = w.Close()
+	if err != nil {
+		log.Println("[SMTP ERROR Close Failed]:", err)
 		return err
 	}
 
