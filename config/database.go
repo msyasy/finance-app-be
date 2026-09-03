5ba7739a0105a29363267cbd9291e3fb5ce4ee4b
Muhammad Syaifulloh <msyasy.care@gmail.com>
2026-09-03 12:10:44 +0700

    Fix database connection to use env variable

--- config/database.go
+++ config/database.go
@@ -4,6 +4,7 @@ import (
 	"database/sql"
 	"fmt"
 	"log"
+	"os"
 
 	_ "github.com/lib/pq"
 )
@@ -11,8 +12,14 @@ import (
 var DB *sql.DB
 
 func ConnectDB() {
-	connStr := "host=localhost port=5432 user=postgres password=317505 dbname=finance_db sslmode=disable"
-	
+	// Ambil dari Environment Variable Render (DATABASE_URL)
+	connStr := os.Getenv("DATABASE_URL")
+
+	// Jika DATABASE_URL kosong (saat dijalankan di lokal tanpa .env)
+	if connStr == "" {
+		connStr = "host=localhost port=5432 user=postgres password=317505 dbname=finance_db sslmode=disable"
+	}
+
 	var err error
 	DB, err = sql.Open("postgres", connStr)
 	if err != nil {
@@ -24,5 +31,5 @@ func ConnectDB() {
 		log.Fatal("Gagal terhubung ke DB:", err)
 	}
 
-	fmt.Println("Berhasil terhubung ke database PostgreSQL (finance_db)!")
+	fmt.Println("Berhasil terhubung ke database PostgreSQL!")
 }
